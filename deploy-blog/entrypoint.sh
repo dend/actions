981641dfcc5430e05e65abb90bff017aa1edca8b
Den Delimarsky <1389609+dend@users.noreply.github.com>
2018-12-16 20:20:00 -0800

    Update entrypoint.sh

--- deploy-blog/entrypoint.sh
+++ deploy-blog/entrypoint.sh
@@ -43,6 +43,7 @@ then
   aws s3 sync --acl "public-read" --sse "AES256" public/ s3://$AWS_BUCKET_NAME/ --exclude 'img' --exclude 'js' --exclude 'downloads' --exclude 'css' --exclude 'post'
 
   # Ensure static files are set to cache forever - cache for a month --cache-control "max-age=2592000"
+  aws s3 sync --cache-control "max-age=2592000" --acl "public-read" --sse "AES256" public/images/ s3://$AWS_BUCKET_NAME/img/
   aws s3 sync --cache-control "max-age=2592000" --acl "public-read" --sse "AES256" public/img/ s3://$AWS_BUCKET_NAME/img/
   aws s3 sync --cache-control "max-age=2592000" --acl "public-read" --sse "AES256" public/css/ s3://$AWS_BUCKET_NAME/css/
   # aws s3 sync --cache-control "max-age=2592000" --acl "public-read" --sse "AES256" public/js/ s3://$AWS_BUCKET_NAME/js/
