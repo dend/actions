31ba0b859f715701d715dc038637be0959257fa5
Den Delimarsky <1389609+dend@users.noreply.github.com>
2021-02-15 16:36:46 -0800

    Update entrypoint.sh

--- deploy-github-io/entrypoint.sh
+++ deploy-github-io/entrypoint.sh
@@ -16,22 +16,14 @@ hugo version
 if [ -z ${LOCAL_THEME_GIT_URL+x} ]; then echo "No theme specified."; else git clone --progress --verbose https://$BLOG_DEPLOY_KEY@$LOCAL_THEME_GIT_URL $LOCAL_THEME_LOCATION --depth 1; fi
 git clone --progress --verbose https://$BLOG_DEPLOY_KEY@$BLOG_PUBLISH_URL $BLOG_PUBLISH_LOCATION --depth 1
 
-
 cd $BLOG_FOLDER
 hugo -v
 
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
 
-# find public/css/ -type f \
-#   -name "*.css" ! -name "*.min.*" \
-#   -exec echo {} \; \
-#   -exec uglifycss --output {}.min {} \; \
-#   -exec rm {} \; \
-#   -exec mv {}.min {} \;
-
 echo "Removing content..."
-rm -rf $BLOG_PUBLISH_LOCATION/*
+rm -rf ../$BLOG_PUBLISH_LOCATION/*
 echo "Content removal completed."
 cp -a public/. ../$BLOG_PUBLISH_LOCATION/
 cd ../$BLOG_PUBLISH_LOCATION
